3800481325e6861536a006b39696b4857f10a240
Nathanael Silverman <nathanael.silverman@gmail.com>
2017-08-18 20:27:21 -0700

    Revert "KPoet third pass"
    This reverts commit 5c4dc52079be581ddda300212d75117e80110294.

--- paris-processor/src/main/java/com/airbnb/paris/processor/ParisWriter.kt
+++ paris-processor/src/main/java/com/airbnb/paris/processor/ParisWriter.kt
@@ -3,7 +3,10 @@ package com.airbnb.paris.processor
 import com.airbnb.paris.processor.utils.ClassNames
 import com.airbnb.paris.processor.utils.className
 import com.grosner.kpoet.*
-import com.squareup.javapoet.*
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.CodeBlock
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeName
 import java.io.IOException
 import java.util.*
 import javax.annotation.processing.Filer
@@ -12,79 +15,71 @@ internal object ParisWriter {
 
     @Throws(IOException::class)
     internal fun writeFrom(filer: Filer, styleableClassesInfo: List<StyleableInfo>) {
-        `package paris` {
-            `public final class Paris extends ParisBase` {
+        javaFile(ParisProcessor.PARIS_CLASS_NAME.packageName()) {
+            `public final class`(ParisProcessor.PARIS_CLASS_NAME.simpleName()) {
+                extends(ParisProcessor.PARIS_BASE_CLASS_NAME)
 
                 for ((styleApplierQualifiedName, viewQualifiedName) in ParisProcessor.BUILT_IN_STYLE_APPLIERS) {
                     val styleApplierClassName = styleApplierQualifiedName.className()
-                    `public static void style(view)`(
+                    addMethod(buildStyleMethod(
                             styleApplierClassName.packageName(),
                             styleApplierClassName.simpleName(),
-                            viewQualifiedName.className())
+                            viewQualifiedName.className()))
                 }
 
                 for (styleableClassInfo in styleableClassesInfo) {
-                    `public static void style(view)`(styleableClassInfo)
+                    addMethod(buildStyleMethod(styleableClassInfo))
                 }
 
-                `public static void assertStylesContainSameAttributes(context)`(styleableClassesInfo)
+                addMethod(buildAssertStylesMethod(styleableClassesInfo))
 
                 this
             }
         }.writeTo(filer)
     }
-}
 
-private fun `package paris`(function: () -> TypeSpec): JavaFile {
-    return javaFile(ParisProcessor.PARIS_CLASS_NAME.packageName(), function = function)
-}
-
-private fun `public final class Paris extends ParisBase`(function: TypeMethod): TypeSpec {
-    return `public final class`(ParisProcessor.PARIS_CLASS_NAME.simpleName()) {
-        extends(ParisProcessor.PARIS_BASE_CLASS_NAME)
-        function()
+    private fun buildStyleMethod(styleableClassInfo: StyleableInfo): MethodSpec {
+        return buildStyleMethod(
+                styleableClassInfo.elementPackageName,
+                String.format(Locale.US, ParisProcessor.STYLE_APPLIER_CLASS_NAME_FORMAT, styleableClassInfo.elementName),
+                TypeName.get(styleableClassInfo.elementType))
     }
-}
 
-private fun TypeSpec.Builder.`public static void style(view)`(styleableClassInfo: StyleableInfo): TypeSpec.Builder {
-    return `public static void style(view)`(
-            styleableClassInfo.elementPackageName,
-            String.format(Locale.US, ParisProcessor.STYLE_APPLIER_CLASS_NAME_FORMAT, styleableClassInfo.elementName),
-            TypeName.get(styleableClassInfo.elementType))
-}
-
-private fun TypeSpec.Builder.`public static void style(view)`(styleApplierPackageName: String, styleApplierSimpleName: String, viewParameterTypeName: TypeName): TypeSpec.Builder {
-    val styleApplierClassName = ClassName.get(styleApplierPackageName, styleApplierSimpleName)
-    return `public static`(styleApplierClassName, "style", param(viewParameterTypeName, "view")) {
-        `return`("process(new \$T(view))", styleApplierClassName)
-    }
-}
-
-private fun TypeSpec.Builder.`public static void assertStylesContainSameAttributes(context)`(styleableClassesInfo: List<StyleableInfo>): TypeSpec.Builder {
-    return `public static`(TypeName.VOID, "assertStylesContainSameAttributes", param(ClassNames.ANDROID_CONTEXT, "context")) {
-        javadoc("For debugging")
-
-        for (styleableClassInfo in styleableClassesInfo) {
-            if (styleableClassInfo.styles.size > 1) {
-                statement("\$T \$T = new \$T(context)", styleableClassInfo.elementType, styleableClassInfo.elementType, styleableClassInfo.elementType)
-
-                val styleVarargCodeBuilder = CodeBlock.builder()
-                for ((i, style) in styleableClassInfo.styles.withIndex()) {
-                    if (i > 0) {
-                        styleVarargCodeBuilder.add(", ")
-                    }
-                    styleVarargCodeBuilder.add("new \$T(\$L)",
-                            ParisProcessor.STYLE_CLASS_NAME, style.androidResourceId.code)
-                }
-
-                val assertEqualAttributesCode = CodeBlock.of("\$T.Companion.assertSameAttributes(style(\$T), \$L);\n",
-                        ParisProcessor.STYLE_APPLIER_UTILS_CLASS_NAME,
-                        styleableClassInfo.elementType,
-                        styleVarargCodeBuilder.build())
-                addCode(assertEqualAttributesCode)
-            }
+    private fun buildStyleMethod(styleApplierPackageName: String, styleApplierSimpleName: String, viewParameterTypeName: TypeName): MethodSpec {
+        val styleApplierClassName = ClassName.get(
+                styleApplierPackageName,
+                styleApplierSimpleName)
+        return `public static`(styleApplierClassName, "style", param(viewParameterTypeName, "view")) {
+            `return`("process(new \$T(view))", styleApplierClassName)
         }
+    }
 
-        this
+    private fun buildAssertStylesMethod(styleableClassesInfo: List<StyleableInfo>): MethodSpec {
+        return `public static`(TypeName.VOID, "assertStylesContainSameAttributes", param(ClassNames.ANDROID_CONTEXT, "context")) {
+            javadoc("For debugging")
+
+            for (styleableClassInfo in styleableClassesInfo) {
+                if (styleableClassInfo.styles.size > 1) {
+                    statement("\$T \$T = new \$T(context)", styleableClassInfo.elementType, styleableClassInfo.elementType, styleableClassInfo.elementType)
+
+                    val styleVarargCodeBuilder = CodeBlock.builder()
+                    for ((i, style) in styleableClassInfo.styles.withIndex()) {
+                        if (i > 0) {
+                            styleVarargCodeBuilder.add(", ")
+                        }
+                        styleVarargCodeBuilder.add("new \$T(\$L)",
+                                ParisProcessor.STYLE_CLASS_NAME, style.androidResourceId.code)
+                    }
+
+                    val assertEqualAttributesCode = CodeBlock.of("\$T.Companion.assertSameAttributes(style(\$T), \$L);\n",
+                            ParisProcessor.STYLE_APPLIER_UTILS_CLASS_NAME,
+                            styleableClassInfo.elementType,
+                            styleVarargCodeBuilder.build())
+                    addCode(assertEqualAttributesCode)
+                }
+            }
+
+            this
+        }
     }
 }
--- paris-processor/src/main/java/com/airbnb/paris/processor/StyleAppliersWriter.kt
+++ paris-processor/src/main/java/com/airbnb/paris/processor/StyleAppliersWriter.kt
@@ -5,10 +5,12 @@ import com.airbnb.paris.processor.utils.ClassNames
 import com.airbnb.paris.processor.utils.asTypeElement
 import com.airbnb.paris.processor.utils.invoke
 import com.grosner.kpoet.*
-import com.squareup.javapoet.*
+import com.squareup.javapoet.ArrayTypeName
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.ParameterizedTypeName
+import com.squareup.javapoet.TypeName
 import java.io.IOException
 import javax.annotation.processing.Filer
-import javax.lang.model.type.TypeMirror
 import javax.lang.model.util.Types
 
 // TODO  Add @UiThread annotation to StyleApplier classes
@@ -29,8 +31,8 @@ internal object StyleAppliersWriter {
         javaFile(styleApplierClassName.packageName()) {
             `public final class`(styleApplierClassName.simpleName()) {
                 extends(ParameterizedTypeName.get(ParisProcessor.STYLE_APPLIER_CLASS_NAME, styleApplierClassName, TypeName.get(styleableInfo.elementType)))
-
-                `public StyleApplier(view)`(styleableInfo.elementType) {
+                constructor(param(TypeName.get(styleableInfo.elementType), "view")) {
+                    modifiers(public)
                     statement("super(view)")
                 }
 
@@ -45,23 +47,26 @@ internal object StyleAppliersWriter {
                     }
                     val rClassName = arbitraryResId.className!!.enclosingClassName()
 
-                    `protected int attributes()` {
+                    protected(ArrayTypeName.of(Integer.TYPE), "attributes") {
+                        `@`(Override::class)
                         statement("return \$T.styleable.\$L", rClassName, styleableInfo.styleableResourceName)
                     }
 
-                    `protected void processAttributes(style, typedArrayWrapper)`(styleableInfo.styleableFields, styleableInfo.attrs)
+                    addMethod(buildProcessAttributesMethod(styleableInfo.styleableFields, styleableInfo.attrs))
                 }
 
                 val parentStyleApplierClassName = styleablesTree.findStyleApplier(
                         typeUtils,
                         styleablesInfo,
                         styleableInfo.elementType.asTypeElement(typeUtils).superclass.asTypeElement(typeUtils))
-                `protected void applyParent(style)` {
+                protected(TypeName.VOID, "applyParent", param(ParisProcessor.STYLE_CLASS_NAME, "style")) {
+                    `@`(Override::class)
                     statement("new \$T(getView()).apply(style)", parentStyleApplierClassName)
                 }
 
                 if (styleableInfo.dependencies.isNotEmpty()) {
-                    `protected void applyDependencies(style)` {
+                    protected(TypeName.VOID, "applyDependencies", param(ParisProcessor.STYLE_CLASS_NAME, "style")) {
+                        `@`(Override::class)
                         for (dependency in styleableInfo.dependencies) {
                             statement("new \$T(getView()).apply(style)", dependency)
                         }
@@ -74,13 +79,13 @@ internal object StyleAppliersWriter {
                             typeUtils,
                             styleablesInfo,
                             styleableFieldInfo.elementType.asTypeElement(typeUtils))
-                    `public StyleApplier styleableField()`(subStyleApplierClassName, styleableFieldInfo.elementName) {
+                    public(subStyleApplierClassName, styleableFieldInfo.elementName) {
                         `return`("new \$T(getView().${styleableFieldInfo.elementName})", subStyleApplierClassName)
                     }
                 }
 
                 for (styleInfo in styleableInfo.styles) {
-                    `public StyleApplier applyStyle()`(styleApplierClassName, styleInfo.name) {
+                    public(styleApplierClassName, "apply${styleInfo.name.capitalize()}") {
                         `return`("apply(\$L)", styleInfo.androidResourceId.code)
                     }
                 }
@@ -89,45 +94,33 @@ internal object StyleAppliersWriter {
             }
         }.writeTo(filer)
     }
-}
 
-private fun TypeSpec.Builder.`public StyleApplier(view)`(customViewType: TypeMirror, function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return constructor(param(TypeName.get(customViewType), "view")) {
-        modifiers(public)
-        function()
-    }
-}
+    private fun buildProcessAttributesMethod(styleableFields: List<StyleableFieldInfo>, attrs: List<AttrInfo>): MethodSpec {
+        return protected(TypeName.VOID, "processAttributes",
+                param(ParisProcessor.STYLE_CLASS_NAME, "style"), param(ParisProcessor.TYPED_ARRAY_WRAPPER_CLASS_NAME, "a")) {
+            `@`(Override::class)
 
-private fun TypeSpec.Builder.`protected int attributes()`(function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return protected(ArrayTypeName.of(Integer.TYPE), "attributes") {
-        `@`(Override::class)
-        function()
-    }
-}
+            statement("\$T res = getView().getContext().getResources()", ClassNames.ANDROID_RESOURCES)
 
-private fun TypeSpec.Builder.`protected void processAttributes(style, typedArrayWrapper)`(styleableFields: List<StyleableFieldInfo>, attrs: List<AttrInfo>): TypeSpec.Builder {
-    fun addStatement(methodSpecBuilder: MethodSpec.Builder, format: Format,
-                     elementName: String, isForDefaultValue: Boolean, statement: String,
-                     androidResourceId: AndroidResourceId, isElementStyleable: Boolean): MethodSpec.Builder {
-        val from = if (isForDefaultValue) "res" else "a"
-        return methodSpecBuilder {
-            if (isElementStyleable) {
-                statement("subStyle = new \$T($from.$statement)", ParisProcessor.STYLE_CLASS_NAME, androidResourceId.code)
-                statement("subStyle.setDebugListener(style.getDebugListener())")
-                statement("\$T.style(getView().\$N).apply(subStyle)", ParisProcessor.PARIS_CLASS_NAME, elementName)
-            } else {
-                if (isForDefaultValue && format == Format.RESOURCE_ID) {
-                    // The parameter is the resource id
-                    statement("getView().\$N(\$L)", elementName, androidResourceId.code)
-                } else {
-                    statement("getView().\$N($from.$statement)", elementName, androidResourceId.code)
-                }
+            if (styleableFields.isNotEmpty()) {
+                statement("\$T subStyle", ParisProcessor.STYLE_CLASS_NAME)
             }
+
+            for (styleableField in styleableFields) {
+                addControlFlow(this, Format.RESOURCE_ID, styleableField.elementName,
+                        styleableField.styleableResId, styleableField.defaultValueResId, true)
+            }
+
+            for (attr in attrs) {
+                addControlFlow(this, attr.targetFormat, attr.elementName,
+                        attr.styleableResId, attr.defaultValueResId, false)
+            }
+
             this
         }
     }
 
-    fun addControlFlow(methodBuilder: MethodSpec.Builder, format: Format,
+    private fun addControlFlow(methodBuilder: MethodSpec.Builder, format: Format,
                                elementName: String, styleableResId: AndroidResourceId,
                                defaultValueResId: AndroidResourceId?, isElementStyleable: Boolean) {
         methodBuilder {
@@ -144,52 +137,24 @@ private fun TypeSpec.Builder.`protected void processAttributes(style, typedArray
         }
     }
 
-    return protected(TypeName.VOID, "processAttributes",
-            param(ParisProcessor.STYLE_CLASS_NAME, "style"), param(ParisProcessor.TYPED_ARRAY_WRAPPER_CLASS_NAME, "a")) {
-        `@`(Override::class)
-
-        statement("\$T res = getView().getContext().getResources()", ClassNames.ANDROID_RESOURCES)
-
-        if (styleableFields.isNotEmpty()) {
-            statement("\$T subStyle", ParisProcessor.STYLE_CLASS_NAME)
+    private fun addStatement(methodSpecBuilder: MethodSpec.Builder, format: Format,
+                             elementName: String, isForDefaultValue: Boolean, statement: String,
+                             androidResourceId: AndroidResourceId, isElementStyleable: Boolean): MethodSpec.Builder {
+        val from = if (isForDefaultValue) "res" else "a"
+        return methodSpecBuilder {
+            if (isElementStyleable) {
+                statement("subStyle = new \$T($from.$statement)", ParisProcessor.STYLE_CLASS_NAME, androidResourceId.code)
+                statement("subStyle.setDebugListener(style.getDebugListener())")
+                statement("\$T.style(getView().\$N).apply(subStyle)", ParisProcessor.PARIS_CLASS_NAME, elementName)
+            } else {
+                if (isForDefaultValue && format == Format.RESOURCE_ID) {
+                    // The parameter is the resource id
+                    statement("getView().\$N(\$L)", elementName, androidResourceId.code)
+                } else {
+                    statement("getView().\$N($from.$statement)", elementName, androidResourceId.code)
+                }
+            }
+            this
         }
-
-        for (styleableField in styleableFields) {
-            addControlFlow(this, Format.RESOURCE_ID, styleableField.elementName,
-                    styleableField.styleableResId, styleableField.defaultValueResId, true)
-        }
-
-        for (attr in attrs) {
-            addControlFlow(this, attr.targetFormat, attr.elementName,
-                    attr.styleableResId, attr.defaultValueResId, false)
-        }
-
-        this
-    }
-}
-
-private fun TypeSpec.Builder.`protected void applyParent(style)`(function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return protected(TypeName.VOID, "applyParent", param(ParisProcessor.STYLE_CLASS_NAME, "style")) {
-        `@`(Override::class)
-        function()
-    }
-}
-
-private fun TypeSpec.Builder.`protected void applyDependencies(style)`(function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return protected(TypeName.VOID, "applyDependencies", param(ParisProcessor.STYLE_CLASS_NAME, "style")) {
-        `@`(Override::class)
-        function()
-    }
-}
-
-private fun TypeSpec.Builder.`public StyleApplier styleableField()`(subStyleApplierClassName: ClassName, styleableFieldName: String, function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return public(subStyleApplierClassName, styleableFieldName) {
-        function()
-    }
-}
-
-private fun TypeSpec.Builder.`public StyleApplier applyStyle()`(styleApplierClassName: ClassName, styleName: String, function: MethodSpec.Builder.() -> MethodSpec.Builder): TypeSpec.Builder {
-    return public(styleApplierClassName, "apply${styleName.capitalize()}") {
-        function()
     }
 }
