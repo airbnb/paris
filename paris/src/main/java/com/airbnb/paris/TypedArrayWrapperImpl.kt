676f3500fcf8a99d49706e5e6af5661d3f73be28
Nathanael Silverman <nathanael.silverman@gmail.com>
2017-08-18 20:45:00 -0700

    TypedArrayWrapperImpl: use expression body

--- paris/src/main/java/com/airbnb/paris/TypedArrayWrapperImpl.kt
+++ paris/src/main/java/com/airbnb/paris/TypedArrayWrapperImpl.kt
@@ -13,73 +13,45 @@ internal class TypedArrayWrapperImpl constructor(val typedArray: TypedArray) : T
      */
     private val NULL_RESOURCE_IDS = HashSet(Arrays.asList(R.anim.null_, R.color.null_, R.drawable.null_))
 
-    override fun isNull(index: Int): Boolean {
-        return NULL_RESOURCE_IDS.contains(typedArray.getResourceId(index, 0))
-    }
+    override fun isNull(index: Int): Boolean =
+            NULL_RESOURCE_IDS.contains(typedArray.getResourceId(index, 0))
 
-    override fun getIndexCount(): Int {
-        return typedArray.indexCount
-    }
+    override fun getIndexCount(): Int = typedArray.indexCount
 
-    override fun getIndex(at: Int): Int {
-        return typedArray.getIndex(at)
-    }
+    override fun getIndex(at: Int): Int = typedArray.getIndex(at)
 
-    override fun hasValue(index: Int): Boolean {
-        return typedArray.hasValue(index)
-    }
+    override fun hasValue(index: Int): Boolean = typedArray.hasValue(index)
 
-    override fun getBoolean(index: Int, defValue: Boolean): Boolean {
-        return typedArray.getBoolean(index, defValue)
-    }
+    override fun getBoolean(index: Int, defValue: Boolean): Boolean =
+            typedArray.getBoolean(index, defValue)
 
-    override fun getColor(index: Int, defValue: Int): Int {
-        return typedArray.getColor(index, defValue)
-    }
+    override fun getColor(index: Int, defValue: Int): Int = typedArray.getColor(index, defValue)
 
-    override fun getColorStateList(index: Int): ColorStateList {
-        return typedArray.getColorStateList(index)
-    }
+    override fun getColorStateList(index: Int): ColorStateList = typedArray.getColorStateList(index)
 
-    override fun getDimensionPixelSize(index: Int, defValue: Int): Int {
-        return typedArray.getDimensionPixelSize(index, defValue)
-    }
+    override fun getDimensionPixelSize(index: Int, defValue: Int): Int =
+            typedArray.getDimensionPixelSize(index, defValue)
 
-    override fun getDrawable(index: Int): Drawable {
-        return typedArray.getDrawable(index)
-    }
+    override fun getDrawable(index: Int): Drawable = typedArray.getDrawable(index)
 
-    override fun getFloat(index: Int, defValue: Float): Float {
-        return typedArray.getFloat(index, defValue)
-    }
+    override fun getFloat(index: Int, defValue: Float): Float = typedArray.getFloat(index, defValue)
 
-    override fun getFraction(index: Int, base: Int, pbase: Int, defValue: Float): Float {
-        return typedArray.getFraction(index, base, pbase, defValue)
-    }
+    override fun getFraction(index: Int, base: Int, pbase: Int, defValue: Float): Float =
+            typedArray.getFraction(index, base, pbase, defValue)
 
-    override fun getInt(index: Int, defValue: Int): Int {
-        return typedArray.getInt(index, defValue)
-    }
+    override fun getInt(index: Int, defValue: Int): Int = typedArray.getInt(index, defValue)
 
-    override fun getLayoutDimension(index: Int, defValue: Int): Int {
-        return typedArray.getLayoutDimension(index, defValue)
-    }
+    override fun getLayoutDimension(index: Int, defValue: Int): Int =
+            typedArray.getLayoutDimension(index, defValue)
 
-    override fun getResourceId(index: Int, defValue: Int): Int {
-        return if (isNull(index)) 0 else typedArray.getResourceId(index, 0)
-    }
+    override fun getResourceId(index: Int, defValue: Int): Int =
+            if (isNull(index)) 0 else typedArray.getResourceId(index, 0)
 
-    override fun getString(index: Int): String {
-        return typedArray.getString(index)
-    }
+    override fun getString(index: Int): String = typedArray.getString(index)
 
-    override fun getText(index: Int): CharSequence {
-        return typedArray.getText(index)
-    }
+    override fun getText(index: Int): CharSequence = typedArray.getText(index)
 
-    override fun getTextArray(index: Int): Array<CharSequence> {
-        return typedArray.getTextArray(index)
-    }
+    override fun getTextArray(index: Int): Array<CharSequence> = typedArray.getTextArray(index)
 
     override fun recycle() {
         typedArray.recycle()
