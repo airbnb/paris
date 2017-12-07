f75645e4c417d6b166c8718ce324ae3979f6e70e
Nathanael Silverman <nathanael.silverman@gmail.com>
2017-12-06 19:21:58 -0800

    Update tests

--- paris-test/src/test/resources/styles/Paris.java
+++ paris-test/src/test/resources/styles/Paris.java
@@ -57,6 +57,6 @@ public final class Paris {
    * For debugging */
   public static void assertStylesContainSameAttributes(Context context) {
     MyView MyView = new MyView(context);
-    StyleApplierUtils.Companion.assertSameAttributes(style(MyView), new MyViewStyleApplier.StyleBuilder().addRedStyle().build(), new MyViewStyleApplier.StyleBuilder().addGreenStyle().build(), new MyViewStyleApplier.StyleBuilder().addBlue().build(), new MyViewStyleApplier.StyleBuilder().addDefault().build());
+    StyleApplierUtils.Companion.assertSameAttributes(style(MyView), new MyViewStyleApplier.StyleBuilder().addRed().build(), new MyViewStyleApplier.StyleBuilder().addGreen().build(), new MyViewStyleApplier.StyleBuilder().addBlue().build(), new MyViewStyleApplier.StyleBuilder().addDefault().build());
   }
 }
