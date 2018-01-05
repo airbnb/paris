ee92fa8cb1fd6c98d0cdf6dbab9a4170acc1629e
Robson Razafindramary <rz.robsn@gmail.com>
2018-01-04 23:16:19 -0500

    extracted reusable code in StyleConverterTest

--- paris/src/androidTest/java/com/airbnb/paris/spannables/StyleConverterTest.kt
+++ paris/src/androidTest/java/com/airbnb/paris/spannables/StyleConverterTest.kt
@@ -7,16 +7,17 @@ import android.text.style.AbsoluteSizeSpan
 import android.text.style.ForegroundColorSpan
 import com.airbnb.paris.attribute_values.ColorValue
 import com.airbnb.paris.styles.ProgrammaticStyle
+import com.airbnb.paris.styles.Style
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.reflect.KClass
 
 @RunWith(AndroidJUnit4::class)
 class StyleConverterTest {
 
     private val converter = StyleConverter(InstrumentationRegistry.getTargetContext()!!)
-    private val helloWorld = "Hello World"
 
     @Test
     fun textSize(){
@@ -25,17 +26,9 @@ class StyleConverterTest {
                 .put(android.R.attr.textSize, 30)
                 .build()
 
-        val spanned = converter.createSpannable(helloWorld, setOf(StyleConverter.MarkupItem(IntRange(3, 5), bigTextStyle)))
-
-        val spans = spanned.getSpans(0, helloWorld.length, AbsoluteSizeSpan::class.java)
-        assertThat(spans.size, equalTo(1))
-
-        val span = spans[0]
-        assertThat(span.size, equalTo(30))
-        assertThat(spanned.getSpanStart(span), equalTo(3))
-        assertThat(spanned.getSpanEnd(span), equalTo(5))
-        assertThat(spanned.getSpans(0, 2, Object::class.java), equalTo(emptyArray()))
-        assertThat(spanned.getSpans(6, 11, Object::class.java), equalTo(emptyArray()))
+        assertStyleAppliesCorrectlyOnSampleString(bigTextStyle, AbsoluteSizeSpan::class) {
+            assertThat(it.size, equalTo(30))
+        }
     }
 
     @Test
@@ -45,13 +38,23 @@ class StyleConverterTest {
                 .put(android.R.attr.textColor, ColorValue(Color.CYAN))
                 .build()
 
-        val spanned = converter.createSpannable(helloWorld, setOf(StyleConverter.MarkupItem(IntRange(3, 5), cyanTextStyle)))
+        assertStyleAppliesCorrectlyOnSampleString(cyanTextStyle, ForegroundColorSpan::class) {
+            assertThat(it.foregroundColor, equalTo(Color.CYAN))
+        }
+    }
 
-        val spans = spanned.getSpans(0, helloWorld.length, ForegroundColorSpan::class.java)
+    private fun <T: Any> assertStyleAppliesCorrectlyOnSampleString(
+            style: Style,
+            spanClass: KClass<T>,
+            spanAssertions: ((T) -> Unit)) {
+
+        val sampleString = "Hello World"
+        val spanned = converter.createSpannable(sampleString, setOf(StyleConverter.MarkupItem(IntRange(3, 5), style)))
+        val spans = spanned.getSpans(0, sampleString.length, spanClass.java)
         assertThat(spans.size, equalTo(1))
 
         val span = spans[0]
-        assertThat(span.foregroundColor, equalTo(Color.CYAN))
+        spanAssertions(span)
         assertThat(spanned.getSpanStart(span), equalTo(3))
         assertThat(spanned.getSpanEnd(span), equalTo(5))
         assertThat(spanned.getSpans(0, 2, Object::class.java), equalTo(emptyArray()))
