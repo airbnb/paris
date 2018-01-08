87ccd4cf5d64d8fd7bcf7e64c73c364234ce5bdd
Robson Razafindramary <rz.robsn@gmail.com>
2018-01-07 21:24:23 -0500

    _ Created Paris_Spannable attrs array _ Added support for fontFamily, textStyle, and typeFace.

--- paris/src/main/java/com/airbnb/paris/spannables/StyleConverter.kt
+++ paris/src/main/java/com/airbnb/paris/spannables/StyleConverter.kt
@@ -3,9 +3,12 @@ package com.airbnb.paris.spannables
 import android.content.Context
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.AbsoluteSizeSpan
 import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.TypefaceSpan
 import com.airbnb.paris.R
 import com.airbnb.paris.styles.Style
 import com.airbnb.paris.typed_array_wrappers.TypedArrayWrapper
@@ -19,10 +22,11 @@ internal class StyleConverter(val context: Context) {
 
     fun createSpannable(text: String, markup: Set<MarkupItem>): Spanned {
 
-        val string = SpannableString(text)
+        val string = SpannableStringBuilder(text)
 
         markup.forEach { markupItem ->
-            spansFromStyle(markupItem.style).forEach {
+            val style = markupItem.style
+            spansFromStyle(style).forEach {
                 string.setSpan(it, markupItem.range.start, markupItem.range.endInclusive, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
             }
         }
@@ -31,12 +35,15 @@ internal class StyleConverter(val context: Context) {
     }
 
     private fun spansFromStyle(style: Style): List<Any> {
-        val attributes = style.obtainStyledAttributes(context, R.styleable.Paris_TextView)
+        val attributes = style.obtainStyledAttributes(context, R.styleable.Paris_Spannable)
 
-        val textSize = attributes.spanAt(R.styleable.Paris_TextView_android_textSize) { AbsoluteSizeSpan(attributes.getDimensionPixelSize(it)) }
-        val foregroundColor = attributes.spanAt(R.styleable.Paris_TextView_android_textColor) { ForegroundColorSpan(attributes.getColorStateList(it).defaultColor) }
+        val textSize = attributes.spanAt(R.styleable.Paris_Spannable_android_textSize) { AbsoluteSizeSpan(attributes.getDimensionPixelSize(it)) }
+        val foregroundColor = attributes.spanAt(R.styleable.Paris_Spannable_android_textColor) { ForegroundColorSpan(attributes.getColorStateList(it).defaultColor) }
+        val fontFamily = attributes.spanAt(R.styleable.Paris_Spannable_android_fontFamily) { TypefaceSpan(attributes.getString(it)) }
+        val textStyle = attributes.spanAt(R.styleable.Paris_Spannable_android_textStyle) { StyleSpan(attributes.getInt(it)) }
+        val typeFace = attributes.spanAt(R.styleable.Paris_Spannable_android_typeface) { TypefaceSpan(attributes.getString(it)) }
 
-        return listOf(textSize, foregroundColor).filterNotNull()
+        return listOf(textSize, foregroundColor, fontFamily, textStyle, typeFace).filterNotNull()
     }
 
     private fun TypedArrayWrapper.spanAt(index: Int, converter: (Int) -> Any): Any? = if (hasValue(index)) converter(index) else null
