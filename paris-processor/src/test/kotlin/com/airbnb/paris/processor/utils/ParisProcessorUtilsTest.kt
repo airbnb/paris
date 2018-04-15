42be34966f1be1177053a198bbc6f969b53dd599
Nathanael Silverman <nathanael.silverman@gmail.com>
2018-04-14 18:19:21 -0700

    Update kotlintest dependency

--- paris-processor/src/test/kotlin/com/airbnb/paris/processor/utils/ParisProcessorUtilsTest.kt
+++ paris-processor/src/test/kotlin/com/airbnb/paris/processor/utils/ParisProcessorUtilsTest.kt
@@ -1,28 +1,26 @@
 package com.airbnb.paris.processor.utils
 
 import com.airbnb.paris.processor.utils.ParisProcessorUtils.Companion.reformatStyleFieldOrMethodName
-import io.kotlintest.matchers.*
-import io.kotlintest.specs.*
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
 
-class ParisProcessorUtilsTest : StringSpec() {
+class ParisProcessorUtilsTest : StringSpec({
 
-    init {
-        "upper snake case style names should be converted to upper camel" {
-            reformatStyleFieldOrMethodName("MY_RED") shouldBe "MyRed"
-        }
-
-        "lower camel case style names should be converted to upper camel" {
-            reformatStyleFieldOrMethodName("myRed") shouldBe "MyRed"
-        }
-
-        "upper camel case style names shouldn't change" {
-            reformatStyleFieldOrMethodName("MyRed") shouldBe "MyRed"
-        }
-
-        "Style suffix should be removed from style names" {
-            reformatStyleFieldOrMethodName("MY_RED_STYLE") shouldBe "MyRed"
-            reformatStyleFieldOrMethodName("myRedStyle") shouldBe "MyRed"
-            reformatStyleFieldOrMethodName("MyRedStyle") shouldBe "MyRed"
-        }
+    "upper snake case style names should be converted to upper camel" {
+        reformatStyleFieldOrMethodName("MY_RED") shouldBe "MyRed"
     }
-}
+
+    "lower camel case style names should be converted to upper camel" {
+        reformatStyleFieldOrMethodName("myRed") shouldBe "MyRed"
+    }
+
+    "upper camel case style names shouldn't change" {
+        reformatStyleFieldOrMethodName("MyRed") shouldBe "MyRed"
+    }
+
+    "Style suffix should be removed from style names" {
+        reformatStyleFieldOrMethodName("MY_RED_STYLE") shouldBe "MyRed"
+        reformatStyleFieldOrMethodName("myRedStyle") shouldBe "MyRed"
+        reformatStyleFieldOrMethodName("MyRedStyle") shouldBe "MyRed"
+    }
+})
---
+++ paris/src/test/java/com/airbnb/paris/typed_array_wrappers/EmptyTypedArrayWrapperSpec.kt
@@ -0,0 +1,46 @@
+package com.airbnb.paris
+
+import com.airbnb.paris.typed_array_wrappers.EmptyTypedArrayWrapper
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldThrow
+import io.kotlintest.properties.forAll
+import io.kotlintest.specs.StringSpec
+
+class EmptyTypedArrayWrapperSpec : StringSpec({
+
+    val wrapper = EmptyTypedArrayWrapper
+
+    "invalid methods" {
+        forAll { index: Int ->
+            shouldThrow<IllegalStateException> {
+                wrapper.isNull(index)
+            }
+            true
+        }
+
+        forAll { at: Int ->
+            shouldThrow<IllegalStateException> {
+                wrapper.getIndex(at)
+            }
+            true
+        }
+
+        forAll { index: Int ->
+            shouldThrow<IllegalStateException> {
+                wrapper.getBoolean(index)
+            }
+            true
+        }
+
+        // TODO etc
+    }
+
+    "valid methods" {
+        wrapper.getIndexCount() shouldBe 0
+
+        forAll { index: Int -> !wrapper.hasValue(index) }
+
+        // Checks that this doesn't throw
+        wrapper.recycle()
+    }
+})
