a8d41212a3bc6a26a2a491e8fadaba24228eb939
Milosz Lewandowski <milosz.lewandowski@gmail.com>
2018-08-20 19:46:21 +0200

    Add detailed explanation of why we need to shadow ResourcesCompat.getFont() method

--- paris/src/testDebug/java/com/airbnb/paris/utils/ShadowResourcesCompat.kt
+++ paris/src/testDebug/java/com/airbnb/paris/utils/ShadowResourcesCompat.kt
@@ -12,6 +12,16 @@ class ShadowResourcesCompat {
 
     companion object {
 
+        /**
+         * Since Robolectric has some
+         * <a href="https://github.com/robolectric/robolectric/issues/3590">issues</a>
+         * with loading font resources, we need to provide our own method of
+         * constructing Typeface object from font resource. It's enough to create Typeface
+         * object with resource id as a font family name. Since Typeface object equality check
+         * depends on font family name and style id, objects created from the same resource
+         * will be considered equal. And they won't clash with typefaces created from predefined
+         * names.
+         */
         @JvmStatic
         @Implementation
         fun getFont(context: Context, @FontRes id: Int): Typeface {
