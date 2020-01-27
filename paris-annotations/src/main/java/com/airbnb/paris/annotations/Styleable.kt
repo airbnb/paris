63f905b04f197daa251c9c4cbd1ff887e0f5c87f
Ben Schwab <ben.schwab@airbnb.com>
2020-01-27 14:18:39 -0800

    add documentation

--- paris-annotations/src/main/java/com/airbnb/paris/annotations/Styleable.kt
+++ paris-annotations/src/main/java/com/airbnb/paris/annotations/Styleable.kt
@@ -3,7 +3,7 @@ package com.airbnb.paris.annotations
 /**
  * @param value The name of the styleable resource.
  * @param emptyDefaultStyle Set to true if the view does not have a default style.
- * Will only be used if [ParisConfig.namespacedResourcesEnabled] is true.
+ * Will only be used if [ParisConfig.namespacedResourcesEnabled] is true. Default: false.
  */
 @Target(AnnotationTarget.CLASS)
 annotation class Styleable(val value: String = "", val emptyDefaultStyle: Boolean = false)
